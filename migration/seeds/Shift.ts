48d5c9e39d321cfa34e8fc6287dda61c0be6412f
Murilo <murilo.salvador95@gmail.com>
2024-10-30 17:09:35 -0300

    feat: add shift seeder

--- migration/seeds/Shift.ts
+++
@@ -1,8 +0,0 @@
-import { ShiftRepository } from '../../src/repositories/ShiftRepository';
-
-export async function seed() {
-  const shiftRepository = new ShiftRepository();
-  shiftRepository.save({ name: 'Manhã' });
-  shiftRepository.save({ name: 'Tarde' });
-  shiftRepository.save({ name: 'Noite' });
-}
