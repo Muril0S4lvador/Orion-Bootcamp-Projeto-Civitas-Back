d5d9858a90f02ce27d1c9c077ec9893cb0af017c
Murilo <murilo.salvador95@gmail.com>
2024-11-27 13:39:13 -0300

    removendo enum em entidade

--- src/entity/Answer.ts
+++ src/entity/Answer.ts
@@ -1,6 +1,5 @@
 import { Column, CreateDateColumn, Entity, ManyToMany, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 import { PDI } from './PDI';
-import { enumAnswers } from '../models/enums/EnumAnswers';
 import { enumQuestionType } from '../models/enums/EnumQuestionType';
 
 @Entity('answer')
@@ -14,8 +13,8 @@ export class Answer {
     @ManyToMany(() => PDI, pdi => pdi.answers)
     pdis: PDI;
 
-    @Column({ name: 'answer', type: 'enum', enum: enumAnswers })
-    public answerType: enumAnswers;
+    @Column({ type: 'varchar', length: '100', nullable: false })
+    answer: string;
 
     @Column({ name: 'questionType', type: 'enum', enum: enumQuestionType })
     public questionType: enumQuestionType;
