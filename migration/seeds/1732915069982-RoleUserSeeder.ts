294039ee4a23cd2007f0a4df4b59e4e1505affc5
Murilo <murilo.salvador95@gmail.com>
2024-11-29 18:20:46 -0300

    role_user Seeder

---
+++ migration/seeds/1732915069982-RoleUserSeeder.ts
@@ -0,0 +1,30 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class RoleUserSeeder1732915069982 implements MigrationInterface {
+
+    private table = 'role_user';
+    private readonly role_users = [
+        {
+            userId: '1',
+            roleId: '2'
+        },
+        {
+            userId: '2',
+            roleId: '1'
+        },
+        {
+            userId: '3',
+            roleId: '3'
+        }
+    ];
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.manager.insert(this.table, this.role_users);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        const roleIds = this.role_users.map(roleUser => roleUser.roleId);
+        await queryRunner.manager.createQueryBuilder().delete().from(this.table).where('roleId IN (:...roleIds)', { roleIds }).execute();
+    }
+
+}
