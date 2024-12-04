69a47988f2724ce81f62f29b2055e9d140d7fb24
Murilo Salvador <murilo.salvador95@gmail.com>
2024-12-04 14:33:14 -0300

    docs: add documentação aos services

--- src/services/ApplyTypeIndexToAnswersService.ts
+++ src/services/ApplyTypeIndexToAnswersService.ts
@@ -3,6 +3,13 @@ import { PDIAnswer } from '../entity/PDIAnswer';
 import { PDIAnswerRepository } from '../repositories/PDIAnswerRepository';
 import { enumQuestionType } from '../models/enums/EnumQuestionType';
 
+/**
+ * Aplica para todas as respostas um índice e um tipo de questão
+ * @param answersId Id das respostas
+ * @param possibleAnswers Todas as respostas possíveis do banco de dados
+ * @param questionType Tipo da pergunta
+ * @returns Um vetor com as respostas do banco de dados, tipo da questão e seu índice
+ */
 export function applyTypeIndexToAnswersService(answersId: number[], possibleAnswers: Answer[], questionType: enumQuestionType): Partial<PDIAnswer>[] {
     const pdiAnswerRepository: PDIAnswerRepository = new PDIAnswerRepository();
 
--- src/services/PDITransactionService.ts
+++ src/services/PDITransactionService.ts
@@ -3,8 +3,15 @@ import { MysqlDataSource } from '../config/database';
 import { PDI } from '../entity/PDI';
 import { PDIAnswer } from '../entity/PDIAnswer';
 import { createPDIAnswerService } from './CreatePDIAnswerService';
+import { PDITransactionResult } from '../models/interfaces/PDITransactionResult';
 
-export async function PDITransactionService(pdi: PDI, answers: Partial<PDIAnswer>[]) {
+/**
+ * Faz uma transação em conjunto para salvar o PDI e suas respostas no banco de dados simultaneamente
+ * @param pdi PDI a ser adicionado ao banco de dados
+ * @param answers Respostas do PDI
+ * @returns Objeto com as informações do PDI salvo (atributo 'pdi') e suas respostas (atributo 'answers')
+ */
+export async function PDITransactionService(pdi: PDI, answers: Partial<PDIAnswer>[]): Promise<PDITransactionResult> {
     const queryRunner: QueryRunner = MysqlDataSource.createQueryRunner();
 
     await queryRunner.startTransaction();
